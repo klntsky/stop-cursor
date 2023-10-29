a126dfcd4d49c5d160bd51b19afd44dc1db7757f
Vladimir Kalnitsky <klntsky@gmail.com>
2023-10-29 21:36:54 +0400

    Allow moving fasttt

--- main.c
+++ main.c
@@ -14,7 +14,9 @@ int main() {
     XineramaScreenInfo *screens;
     int num_screens;
     int last_screen_id = -1;  // Initially set to an invalid ID
-    int last_x, last_y;       // Store last cursor position relative to the display
+    int last_x;       // Store last cursor position relative to the display
+    int prev_x = 0, prev_y = 0;  // Previous cursor position
+    double speed_x, speed_y;
 
     dpy = XOpenDisplay(NULL);
     if (dpy == NULL) {
@@ -35,6 +37,13 @@ int main() {
         XQueryPointer(dpy, root, &root, &child, &x, &y, &win_x, &win_y, &mask);
         printf("Cursor Position: x=%d, y=%d\n", x, y);
 
+        // Calculate speed based on position difference and fixed time interval
+        speed_x = abs(x - prev_x) / 10.0;  // 10 milliseconds
+        speed_y = abs(y - prev_y) / 10.0;  // 10 milliseconds
+
+        prev_x = x;
+        prev_y = y;
+
         int left_mouse_button_down = mask & Button1Mask;
         int ctrl_key_pressed = mask & ControlMask;
 
@@ -48,23 +57,26 @@ int main() {
             }
         }
 
-        // If either left mouse button is down or CTRL key is pressed, skip the rest of the loop
-        if (left_mouse_button_down || ctrl_key_pressed) {
-            if (ctrl_key_pressed) {
-                last_screen_id = current_screen_id;
-            }
+        // If the left mouse button is down or speed is too high, skip the rest of the loop
+        if (left_mouse_button_down) {
+            usleep(10000);
+            continue;
+        }
+
+        // If CTRL key is pressed, allow cursor to move to another display and update last_screen_id
+        if (ctrl_key_pressed || speed_x >= 10 || speed_y >= 10) {
+            last_screen_id = current_screen_id;
             usleep(10000);
             continue;
         }
 
         if (last_screen_id != -1 && current_screen_id != last_screen_id) {
-            // If there's a change in active screen ID, restore the cursor position
+            // If there's a change in active screen ID and CTRL isn't pressed, restore the x-coordinate of the cursor position
             XWarpPointer(dpy, None, root, 0, 0, 0, 0, last_x, y);
-            printf("Cursor Warped back to: x=%d, y=%d\n", last_x, last_y);
+            printf("Cursor x-coordinate Warped back to: x=%d\n", last_x);
         } else {
-            // Update the last known position and screen ID
+            // Update the last known x-coordinate position and screen ID
             last_x = x;
-            last_y = y;
             last_screen_id = current_screen_id;
         }
 
